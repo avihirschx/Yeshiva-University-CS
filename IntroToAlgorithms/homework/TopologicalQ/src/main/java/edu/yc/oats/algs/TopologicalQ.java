af9706ed0a2ab2485622a81c62f841485c52cf54
Avi Hirsch <adhirsch@mail.yu.edu>
2017-12-07 23:02:36 -0500

    added Queue, updated TopologicalQ

--- IntroToAlgorithms/homework/TopologicalQ/src/main/java/edu/yc/oats/algs/TopologicalQ.java
+++ IntroToAlgorithms/homework/TopologicalQ/src/main/java/edu/yc/oats/algs/TopologicalQ.java
@@ -1,8 +1,17 @@
 package edu.yc.oats.algs;
 
 public class TopologicalQ {
+	private Iterable<Integer> order;
 
 	public TopologicalQ(Digraph G) {
 
 	}
+
+	public boolean hasOrder() {
+		return order != null;
+	}
+
+	public Iterable<Integer> order() {
+		return order;
+	} 
 }
